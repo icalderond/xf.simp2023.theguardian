8b1e521dbdb8d45e057a0a98a7c3595062be4f9c
Israel Calderon de la Cruz <icalderond@outlook.com>
2023-10-01 17:18:16 -0600

    + event trigger and lamda

--- xf.simp2023.notes/xf.simp2023.notes/MainPage.xaml.cs
+++ xf.simp2023.notes/xf.simp2023.notes/MainPage.xaml.cs
@@ -4,13 +4,19 @@ using System.Collections.Generic;
 using Xamarin.Forms;
 
 namespace xf.simp2023.notes
-{	
-	public partial class MainPage : ContentPage
-	{	
-		public MainPage ()
-		{
-			InitializeComponent ();
-		}
-	}
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            //btnLogIn.Clicked += Button_Clicked;
+        }
+
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            App.Current.MainPage.DisplayAlert("Message", $"Bienvenido usuario {entryUser.Text}", "Ok");
+        }
+    }
 }
 
