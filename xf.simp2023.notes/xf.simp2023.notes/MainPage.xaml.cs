d273e4c6778643611295ccdb83deec75b549d591
Israel Calderon de la Cruz <icalderond@outlook.com>
2023-10-01 14:58:39 -0600

    First commit

---
+++ xf.simp2023.notes/xf.simp2023.notes/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace xf.simp2023.notes
+{	
+	public partial class MainPage : ContentPage
+	{	
+		public MainPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
