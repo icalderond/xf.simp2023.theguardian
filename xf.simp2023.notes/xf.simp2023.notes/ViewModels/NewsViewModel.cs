bdf9e99a21e139ee3e833d0c5f79ee4922e1c4c5
Israel Calderon de la Cruz <icalderond@outlook.com>
2023-10-02 03:31:21 -0600

    + implement service

---
+++ xf.simp2023.notes/xf.simp2023.notes/ViewModels/NewsViewModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using xf.simp2023.notes.Helpers;
+using xf.simp2023.notes.Models;
+using xf.simp2023.notes.Services;
+
+namespace xf.simp2023.notes.ViewModels
+{
+    public class NewsViewModel : ViewModelBase
+    {
+        public NewsViewModel()
+        {
+            _ = Initialialize();
+        }
+
+        private ObservableCollection<FeedNew> _FeedNews;
+        public ObservableCollection<FeedNew> FeedNews
+        {
+            get => _FeedNews;
+            set
+            {
+                _FeedNews = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private FeedNew _MainFeedNew;
+        public FeedNew MainFeedNew
+        {
+            get => _MainFeedNew;
+            set
+            {
+                _MainFeedNew = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool _IsBusy;
+        public bool IsBusy
+        {
+            get => _IsBusy;
+            set
+            {
+                _IsBusy = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private async Task Initialialize()
+        {
+            try
+            {
+                IsBusy = true;
+                RSSService rSSService = new RSSService();
+                var resultList = await rSSService.GetNews();
+
+                MainFeedNew = resultList.FirstOrDefault();
+                FeedNews = new ObservableCollection<FeedNew>(resultList.GetRange(1, resultList.Count - 1));
+            }
+            catch (Exception ex)
+            {
+                await App.Current.MainPage.DisplayAlert("Error", ex.Message, "Ok");
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+    }
+}
+
---
+++ xf.simp2023.notes/xf.simp2023.notes/Views/NewsPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using xf.simp2023.notes.ViewModels;
+
+namespace xf.simp2023.notes.Views
+{
+    public partial class NewsPage : ContentPage
+    {
+        public NewsPage()
+        {
+            InitializeComponent();
+            this.BindingContext = new NewsViewModel();
+        }
+    }
+}
