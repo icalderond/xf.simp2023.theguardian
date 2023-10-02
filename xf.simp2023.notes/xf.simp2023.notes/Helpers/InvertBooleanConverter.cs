2d3a885503fc810edcd9aea19695173bb0a2b2af
Israel Calderon de la Cruz <icalderond@outlook.com>
2023-10-01 18:47:00 -0600

    IValueConverte, StaticResource and ResourceDictionary

---
+++ xf.simp2023.notes/xf.simp2023.notes/Helpers/InvertBooleanConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace xf.simp2023.notes.Helpers
+{
+    public class InvertBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool newValue)
+            {
+                return !newValue;
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
