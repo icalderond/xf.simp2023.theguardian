41696f2fd8d761e81ade5da8b295bd78082d025d
Israel Calderon de la Cruz <icalderond@outlook.com>
2023-10-02 03:30:23 -0600

    + service for get rss

---
+++ xf.simp2023.notes/xf.simp2023.notes/Models/FeedNew.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace xf.simp2023.notes.Models
+{
+    public class FeedNew
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Link { get; set; }
+        public string UrlImage { get; set; }
+        public string PubDate { get; set; }
+        public string Creator { get; set; }
+        public string Category { get; internal set; }
+    }
+}
+
